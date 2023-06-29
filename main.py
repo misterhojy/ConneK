e5fee7c32db474225145dd67914f257ccdd1f1d5
Hojat <hojat.jaffary@gmail.com>
2023-06-29 00:52:37 -0400

    Optional attributes

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
-from typing import List
+from typing import Optional
+
 
 
 app = FastAPI()
@@ -14,17 +15,17 @@ app = FastAPI()
 # Tag
 class Tag(BaseModel):
     name: str
-    color: str
-    members: list
+    color: Optional[str] = None
+    members: Optional[list] = None
 
 # Contact
 class Contact(BaseModel):
     first_name: str
-    last_name: str
-    image: str
-    phone_number: int
-    reminder: int
-    tags: list
+    last_name: Optional[int] = None
+    image: Optional[str] = None
+    phone_number: Optional[int] = None
+    reminder: Optional[int] = None
+    tags: Optional[list] = None
 
 @app.get("/") # decorator | Should this be login
 def root():
